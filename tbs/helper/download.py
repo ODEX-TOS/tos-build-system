c0df3f4c7fdabbe184552d6f121eec4980b8b5e3
F0xedb <tom@odex.be>
2020-01-06 19:11:11 +0100

    When git clone doesn't work try to chdir to that location

--- tbs/helper/download.py
+++ tbs/helper/download.py
@@ -6,6 +6,5 @@ def downloadRepo(subpath="toslive"):
     """
     result = fd.CMD(["git", "clone", "https://github.com/ODEX-TOS/tos-live.git"]).execute(True)
     if not result.exitcode == 0:
-        logger.log("Something went wrong when trying to download build files", logger.LOG_ERROR)
-        raise Exception(result.stderr)
+        logger.log("Something went wrong when trying to download build files, checking for existing build files", logger.LOG_ERROR)
     os.chdir("tos-live/"+subpath)
